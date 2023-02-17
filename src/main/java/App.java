8b146559122fab3271dda455f4c465d3b6185d89
Lyotina Yuliya <lyotinayuliya@mail.ru>
2023-02-18 02:24:09 +0600

    1802

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,14 +9,10 @@ public class App {
                 (HelloWorld) applicationContext.getBean("helloworld");
         HelloWorld bean2 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
-        System.out.println(bean.getMessage());
         Cat cat =
                 (Cat) applicationContext.getBean("cat");
         Cat cat2 =
                 (Cat) applicationContext.getBean("cat");
-        System.out.println(cat.getMessage());
-        System.out.println(cat.getMessage());
         System.out.println(bean == bean2);
         System.out.println(cat == cat2);
     }
